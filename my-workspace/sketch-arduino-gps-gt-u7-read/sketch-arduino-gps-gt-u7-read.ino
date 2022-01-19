d43786e686060e84c1e3b58c798d0b00886c9ba6
Greg <gregdirst@gmail.com>
2022-01-18 21:10:26 -0600

    Add files via upload

---
+++ my-workspace/sketch-arduino-gps-gt-u7-read/sketch-arduino-gps-gt-u7-read.ino
@@ -0,0 +1,34 @@
+#include "TinyGPSPlus.h"
+#include <SoftwareSerial.h>
+
+SoftwareSerial serial_connection(10, 11); // RX=pin 10, TX=pin 11
+TinyGPSPlus gps;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  serial_connection.begin(9600);
+  Serial.println("GPS Start");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(serial_connection.available())
+  {
+    gps.encode(serial_connection.read());
+  }
+  if(gps.location.isUpdated())
+  {
+    Serial.println("Satellite Count:");
+    Serial.println(gps.satellites.value());
+    Serial.println("Latitude:");
+    Serial.println(gps.location.lat(), 6);
+    Serial.println("Longitude:");
+    Serial.println(gps.location.lng(), 6);
+    Serial.println("Speed MPH:");
+    Serial.println(gps.speed.mph());
+    Serial.println("Altitude Feet:");
+    Serial.println(gps.altitude.feet());
+    Serial.println("");
+  }
+}
