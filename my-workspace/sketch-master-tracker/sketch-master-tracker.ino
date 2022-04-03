23ba92a07045c88e015214bf3c1638e990b9655a
Greg <gregdirst@gmail.com>
2022-04-02 22:39:59 -0500

    Add files via upload

--- my-workspace/sketch-master-tracker/sketch-master-tracker.ino
+++ my-workspace/sketch-master-tracker/sketch-master-tracker.ino
@@ -6,6 +6,7 @@
 // GPS libraries
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
+//#include <Sodaq_DS3231.h>
 
 // SD Card libraries
 #include <SPI.h>
@@ -13,99 +14,136 @@
 
 // BMP180 libraries
 #include "Wire.h"    // imports the wire library for talking over I2C 
-#include "Adafruit_BMP085.h"  // import the Pressure Sensor Library
+#include "Adafruit_BMP085.h"  // import the fPrs Sensor Library
 
 //=================
 // Global variables
 //=================
 
-// Create the serial_connection
-//
-// Connect the GT-U7 GPS Module RX and TX pins to the Arduino
-// digital pins on the board.
-//
 // Connect the RXD pin on the GPS to the Arduino digital 5 pin
 // Connect the TXD pin oo the GPS to the Arduino digital 6 pin
-//
 // pass TXD pin number 6 as the first argument, followed by the RXD pin number 5
-//
 SoftwareSerial serialGPS(6, 5); // serial_conn
 TinyGPSPlus gps;
-File dataFile;
-Adafruit_BMP085 mySensor;  // create sensor object called mySensor
+Adafruit_BMP085 bmp180;  // create sensor object called bmp180
+String sCSV = "";
 
 //===================
 // Main Code Sections
 //===================
 
+//void writeToFile(String fileName, String contentToWrite){
+//    dataFile = SD.open(fileName, FILE_WRITE);
+//    dataFile.println(contentToWrite);
+//    dataFile.close();
+//}
+
+String getTime(){
+  
+  String timeString = "";
+  
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) timeString += F("0");
+    timeString += String(gps.time.hour()) + F(":");
+    
+    if (gps.time.minute() < 10) timeString += F("0");
+    timeString += String(gps.time.minute()) + F(":");
+    
+    if (gps.time.second() < 10) timeString += F("0");
+    timeString += String(gps.time.second());
+//    timeString += String(gps.time.second()) + F(".");
+    
+//    timeString += String(gps.time.centisecond());
+  }
+  else
+  {
+    return F("INVALID");
+  }
+
+  return timeString;
+}
+
 // put your setup code here, to run once:
 void setup() {
+
+  // set the digital pin to the onboard LED:
+  //pinMode(LED_BUILTIN, OUTPUT);
 
   // open serial port
   // over the USB cable
   Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  
   // open gps
   serialGPS.begin(9600);
+  
+  // open bmp180
+  if (!bmp180.begin()) {
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while(1){}
+  }
+  
   // open sd card
-  SD.begin(10);
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
 }
 
 // put your main code here, to run repeatedly:
 void loop() {
 
-  //Serial.println("a");
+  sCSV = "";
 
   // while there is information available to
   // read from the gps module
   //    read the data
-  while(serialGPS.available())
-  {
-    //Serial.println("b");
+  while(serialGPS.available()){
     gps.encode(serialGPS.read());
   }
 
-  //Serial.println("c");
-
-  // any time the gps location changes
-  // read all the individual value we are interested in
-  // format the string using CSV format
-  //
-  // cvs format is a string containing different values separated by a comma...
-  // value1,value2,value3,...
-  //
-  if(gps.location.isUpdated())
-  {
-    Serial.println("d");
+  if(gps.location.isUpdated()){
     
-    // read the values we are interested in and store in a variable
-    Serial.println("gps updated");
-
-    // gps info
-    int numberOfSatellites = gps.satellites.value();
-    float latitude = gps.location.lat();
-    float longitude = gps.location.lng();
-    float speedInMph = gps.speed.mph();
-    float altitudeInFeet = gps.altitude.feet();
-
-    // sensor info
-    float tempC = mySensor.readTemperature(); //  Read degrees C
-    float tempF = tempC*1.8 + 32.; // Convert degrees C to F
-    float pressure=mySensor.readPressure(); //Read Pressure
-
-    // store the values we read above in a string using CSV format
+    if (gps.location.isValid()){
     
-    // numberOfSatellites,latitude,longitude,speedInMph,altitudeInFeet,tempC,tempF,pressure
-    String csvString = String(numberOfSatellites, DEC) + "," + String(latitude, 12) + "," + String(longitude, 12) + "," + String(speedInMph, 4) + "," + String(altitudeInFeet, 6) + "," + tempC + "," + tempF + "," + pressure;
-    Serial.println(csvString);
-    
-    // open the file. note that only one file can be open at a time,
-    // so you have to close this one before opening another.
-    dataFile = SD.open("flight.log", FILE_WRITE);
-    dataFile.println(csvString);
+      // read the values we are interested in and store in a variable
+
+      // time info
+      String sTime = getTime();
   
-    // always close the file when done
-    dataFile.close();
+      // gps info
+      String sSat = String(gps.satellites.value(), DEC);
+      String sLat = String(gps.location.lat(),6);
+      String sLng = String(gps.location.lng(),6);
+      String sSpd = String(gps.speed.mph());
+      String sAlt = String(gps.altitude.feet());
 
-    delay(1000);
+  
+      // sensor info
+      float fCel = bmp180.readTemperature(); //  Read degrees C
+      float fFer = fCel*1.8 + 32.; // Convert degrees C to F
+      String sCel = String(fCel, 6);
+      String sFer = String(fFer, 6);
+      // TODO: convert pressure
+      String sPrs = String(bmp180.readPressure(), 6);
+      
+      // store the values we read above in a string using CSV format
+      
+      // time,sat,lat,lng,alt,spd,cel,fehr,press,hum
+      // TODO: add humidity
+      sCSV = sTime + "," + sSat + "," + sLat + "," + sLng + "," + sAlt + "," + sSpd + "," + sCel + "," + sFer + "," + sPrs;
+//      Serial.println(sCSV);
+    }
+  }
+
+  if(sCSV != ""){
+    Serial.println(sCSV);
+//    dataFile = SD.open("flight.log", FILE_WRITE);
+//    dataFile.println(sCSV);
+//    dataFile.close();
+    delay(5000);
   }
 }
