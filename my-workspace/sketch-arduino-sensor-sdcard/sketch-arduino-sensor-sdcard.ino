44d10ef289c0a9d5b099364745246fca8202ee5b
ssamjung2 <44533925+ssamjung2@users.noreply.github.com>
2022-04-25 22:55:15 -0500

    Update sketch-arduino-sensor-sdcard.ino
    You had the LED Pin assigned to CS of the SD card reader.  I moved the LED to a different PIN and added a function to blink the light after a SD card write.
    
    The SD Card module I don't think has a logic level converter between 3.3v and 5v, thus it isn't recognized by the Uno R3.  To try and fix this issue, I added two statements to apply pull up voltage to the CS pin for the SD Card.
    
      pinMode(10, OUTPUT); // Add this line
      digitalWrite(10, HIGH); // Add this line  
    
    Interfacing Micro SD Card Module with Arduino
    https://lastminuteengineers.com/arduino-micro-sd-card-module-tutorial/
    
    Workaround Arduino SD Card Initialization Error!
    https://embedjournal.com/arduino-sd-card-initialization-failed/

--- my-workspace/sketch-arduino-sensor-sdcard/sketch-arduino-sensor-sdcard.ino
+++ my-workspace/sketch-arduino-sensor-sdcard/sketch-arduino-sensor-sdcard.ino
@@ -1,132 +1,263 @@
+/*
+  GPS Position Logger
+  gps-position-logger.ino
+  Read GPS data from BN-220 or any serial GPS sensor
+  Requires TinyGPS++ Library
+  Save to SD or microSD card
 
-#include <Timezone.h>   // https://github.com/JChristensen/Timezone
+  bmp180 pins
+  VIN - 5v
+  GRD - gnd
+  SCL - A5 analog
+  SDA - A4 analog
 
-// SD card include libraries
-//--------------------------
+  GPS pins
+  TXD - 3 digital
+  RXD - 4 digital
+  GND - gnd
+  VCC - 5v
 
-#include <SD.h> // load the SD library
-#include <SPI.h> // load the SPI library
+  sdcard pins
+  CS - 10 digital
+  SCK - 13 digital
+  MOSI - 11 digital
+  MISO - 12 digital
+  VCC - 5v
+  GND - gnd
 
-// SD card include libraries
-//--------------------------
+  DroneBot Workshop 2021
+  https://dronebotworkshop.com/using-gps-modules/#Build_a_GPS_Position_Logger
+*/
 
-#include "Wire.h"    // imports the wire library for talking over I2C 
-#include "Adafruit_BMP085.h"  // import the Pressure Sensor Library
+// Include required libraries
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <SD.h>
+#include "Adafruit_BMP085.h"
 
-// sensor variables
-//-----------------
-
-Adafruit_BMP085 mySensor;  // create sensor object called mySensor
-
+// BMP sensor
+Adafruit_BMP085 bmp180;
 float tempC;  // Variable for holding temp in C
 float tempF;  // Variable for holding temp in F
 float pressure; //Variable for holding pressure reading
 
-// sdcard variables
-//-----------------
+// GPS Connections
+static const int RXPin = 4, TXPin = 3;
 
-File myFile;
-int digitalPin=4; // this number needs to match the digital pin number CS is connected to
+// GPS Baud rate (change if required)
+static const uint32_t GPSBaud = 9600;
 
-void setup(){
+// SD Card Select pin
+const int chipSelect = 10;
 
-  // initialize the serial monitor
-  //------------------------------
+// Write LED
+const int recLED = 7;
+
+// String to hold GPS data
+String gpstext;
+
+// GPS write delay counter variables
+// Change gpsttlcount as required
+int gpscount = 0;
+int gpsttlcount = 30;
+
+// TinyGPS++ object
+TinyGPSPlus gps;
+
+// SoftwareSerial connection to the GPS device
+SoftwareSerial ss(TXPin, RXPin);
+
+void blinky()
+{
+  // Blink LED so we know we are ready
+  digitalWrite(recLED, HIGH);
+  delay(50);
+  digitalWrite(recLED, LOW);
+  delay(50);
+  digitalWrite(recLED, HIGH);
+  delay(50);
+  digitalWrite(recLED, LOW);
+  delay(50);
+  digitalWrite(recLED, HIGH);
+  delay(50);
+  digitalWrite(recLED, LOW);
   
-    Serial.begin(9600);
-    while (!Serial) {
-      ; // wait for serial port to connect. Needed for native USB port only
-    }
-  
-    // initialize the SD card
-    //-----------------------
-  
-    Serial.print("Initializing SD card...");
-    
-    if (!SD.begin(4)) {
-      Serial.println("initialization failed!");
-      while (1); // why are we doing this? can we just exit?
-    }
-    
-    Serial.println("SD card initialization done.");
-
-  // create a test file
-  //-------------------
-
-    Serial.println("Creating data file...");
-    
-    // open the file. note that only one file can be open at a time,
-    // so you have to close this one before opening another.
-    myFile = SD.open("datafile.log", FILE_WRITE);
-    
-    if (myFile) {
-      Serial.println("data file created.");
-      myFile.close();
-    }
-    else {
-      Serial.println("error opening data file.");
-      while(1);
-    }
-    
-    Serial.println("Initializing the sensor...");
-    
-  // initialize the sensor
-  //----------------------
-
-    Serial.println("Initializing the sensor...");
-    
-    mySensor.begin();   //initialize mySensor
-
-    Serial.println("setup complete.");
-    
-    
 }
 
+void setup()
+{
+  // Set LED pin as output
+  pinMode(recLED, OUTPUT);
 
+  pinMode(10, OUTPUT); // Add this line
+  digitalWrite(10, HIGH); // Add this line  
 
-void loop() {
+  // Start Serial Monitor for debugging
+  Serial.begin(115200);
 
-  // take sensor readings
-  //---------------------
-
-  time_t utc = now();
+  // Start SoftwareSerial
+  ss.begin(GPSBaud);
   
-  tempC = mySensor.readTemperature(); //  Read Temperature
+  // Dispatch incoming characters
+  while (ss.available() > 0)
+  {
+    gps.encode(ss.read());
+    Serial.println("Encoding GPS data");
+  }
+
+  // Initialize SD card
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    //don't do anything more:
+    while (1);
+  }
+  Serial.println("Card initialized.");
+
+  // not sure why, but this had to come 
+  // after sd card for some reason
+  bmp180.begin();
+  
+  // Blink LED so we know we are ready
+  digitalWrite(recLED, HIGH);
+  delay(50);
+  digitalWrite(recLED, LOW);
+  delay(50);
+  digitalWrite(recLED, HIGH);
+  delay(50);
+  digitalWrite(recLED, LOW);
+  delay(50);
+  digitalWrite(recLED, HIGH);
+  delay(50);
+  digitalWrite(recLED, LOW);
+
+}
+
+void loop()
+{
+
+  // Turn off LED
+  digitalWrite(recLED, LOW);
+
+  // See if data available
+  // NMEA data pulls down every 1 second
+  while (ss.available() > 0)
+    gps.encode(ss.read()); 
+     
+  //Serial.println("Reading GPS data");
+
+  if (gps.location.isValid())
+  {
+    // See if we have a complete GPS data string
+    if (getGPSInfo() != "0")
+    {   
+      // Get GPS string
+      gpstext = getGPSInfo();
+
+      if (gpscount == gpsttlcount) 
+      {
+        //Serial.println(F("Writing to SD Card..."));
+        
+        //Open the file on card for writing
+        File dataFile = SD.open("gpslog.csv", FILE_WRITE);
+
+        if (dataFile) 
+        {
+          // If the file is available, write to it and close the file
+          dataFile.println(gpstext);
+          dataFile.close();
+
+          // Serial print GPS string for debugging
+          Serial.println(gpstext);
+          blinky();
+        }
+        // If the file isn't open print an error message for debugging
+        else {
+          Serial.println("error opening datalog.txt");
+        }
+      }
+    } else {
+      Serial.println("Error displaying location.");
+    }
+  }
+  else
+  {
+    Serial.println("GPS location not valid.");
+    //Serial.print("GPS serial overflow:");Serial.println(ss.overflow());  // For GPS serial troubleshooting
+  }
+  // Increment GPS Count
+  gpscount = gpscount + 1;
+  if (gpscount > gpsttlcount) {
+    gpscount = 0;
+  }
+}
+
+// Function to return GPS string
+String getGPSInfo()
+{
+  // Define empty string to hold output
+  String gpsdata = "";
+
+  // Get latitude and longitude
+  if (gps.location.isValid())
+  {
+    gpsdata = String(gps.location.lat(), 6);
+    gpsdata += (",");
+    gpsdata += String(gps.location.lng(), 6);
+    gpsdata += (",");
+  }
+  else
+  {
+    return "0";
+  }
+
+  // Get Date
+  if (gps.date.isValid())
+  {
+    gpsdata += String(gps.date.year());
+    gpsdata += ("-");
+    if (gps.date.month() < 10) gpsdata += ("0");
+    gpsdata += String(gps.date.month());
+    gpsdata += ("-");
+    if (gps.date.day() < 10) gpsdata += ("0");
+    gpsdata += String(gps.date.day());
+  }
+  else
+  {
+    return "0";
+  }
+
+  // Space between date and time
+  gpsdata += (" ");
+
+  // Get time
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) gpsdata += ("0");
+    gpsdata += String(gps.time.hour());
+    gpsdata += (":");
+    if (gps.time.minute() < 10) gpsdata += ("0");
+    gpsdata += String(gps.time.minute());
+    gpsdata += (":");
+    if (gps.time.second() < 10) gpsdata += ("0");
+    gpsdata += String(gps.time.second());
+  }
+  else
+  {
+    return "0";
+  }
+
+  tempC = bmp180.readTemperature(); //  Read Temperature
   tempF = tempC*1.8 + 32.; // Convert degrees C to F
-  pressure=mySensor.readPressure(); //Read Pressure
+  pressure=bmp180.readPressure()/3386.389; //Read Pressure
 
-  // print results to the serial port
-  //---------------------------------
+  gpsdata += (",");
+  gpsdata += String(tempC);
+  gpsdata += (",");
+  gpsdata += String(tempF);
+  gpsdata += (",");
+  gpsdata += String(pressure);
 
-  Serial.print("The time in UTC is: ");
-  Serial.println(utc);
-  Serial.print("The Temp is: "); //Print Your results
-  Serial.print(tempF);
-  Serial.println(" degrees F");
-  Serial.print("The Pressure is: ");
-  Serial.print(pressure);
-  Serial.println(" Pa.");
-  Serial.println("");
-  
-  // write data to data file
-  //------------------------
-  
-  myFile = SD.open("datafile.log", FILE_WRITE);
 
-  if (myFile) {
-    myFile.print("TODO");
-    myFile.print(",");
-    myFile.print(tempC);
-    myFile.print(",");
-    myFile.print(tempF);
-    myFile.print(",");
-    myFile.println(pressure);
-    myFile.close();
-  }
-  else {
-    Serial.println("error opening data file.");
-  }
-  
-  
-  delay(250); //Pause between readings.
+  // Return completed string
+  return gpsdata;
 }
